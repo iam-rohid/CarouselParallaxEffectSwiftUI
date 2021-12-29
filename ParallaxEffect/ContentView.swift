c3700ae8c83ce65a0d8187dcc5c8171c9495335e
Rohidul Islam <rohidul209@gmail.com>
2021-12-29 10:41:01 +0600

    Initial Commit

---
+++ ParallaxEffect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ParallaxEffect
+//
+//  Created by Rohid on 29/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ParallaxEffect/ParallaxEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxEffectApp.swift
+//  ParallaxEffect
+//
+//  Created by Rohid on 29/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
